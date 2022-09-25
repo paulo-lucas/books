499fe106eae056b8814b9a2729ea50394d59bf6c
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-25 19:19:47 -0300

    [feat] volume interfaces

---
+++ app/interfaces/volume.ts
@@ -0,0 +1,48 @@
+export interface VolumeSearchRequest {
+  search?: string;
+  title?: string;
+  author?: string;
+  publisher?: string;
+}
+
+export interface VolumeSearchResponse {
+  kind: string;
+  totalItems: number;
+  items: Array<Volume>;
+}
+
+export interface VolumeInfo {
+  title: string;
+  authors: Array<String>;
+  publisher: string;
+  publishedDate: string;
+  description: string;
+  pageCount: number;
+  categories: Array<string>;
+  maturityRating: string;
+  imageLinks: { smallThumbnail: string; thumbnail: string };
+  language: string;
+  previewLink: string;
+  infoLink: string;
+  canonicalVolumeLink: string;
+}
+
+export interface SaleInfo {
+  country: string;
+  saleability: string;
+  isEbook: boolean;
+}
+
+export interface SearchInfo {
+  textSnippet: string;
+}
+
+export interface Volume {
+  kind: string;
+  id: string;
+  selfLink: string;
+  volumeInfo: VolumeInfo;
+  saleInfo: SaleInfo;
+  accessInfo: any;
+  searchInfo: SearchInfo;
+}
