1bf65abd4f2fe5bb4334114d48599bc593bec596
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-25 18:10:35 -0300

    [feat] toggle theme button

--- app/components/ToggleTheme.tsx
+++ app/components/ToggleTheme.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { useThemeMode, Button } from '@rneui/themed';
+import { Icon, useTheme, useThemeMode } from '@rneui/themed';
 
 export const ToggleTheme = () => {
   const { mode, setMode } = useThemeMode();
+  const {
+    theme: { colors },
+  } = useTheme();
 
-  const toggle = () => setMode(mode === 'light' ? 'dark' : 'light');
+  const isDark = () => mode === 'dark';
+  const toggle = () => setMode(isDark() ? 'light' : 'dark');
 
-  return <Button onPress={toggle} title={mode} />;
+  return (
+    <Icon
+      name={isDark() ? 'moon' : 'sun'}
+      type="feather"
+      color={colors.black}
+      onPress={toggle}
+    />
+  );
 };
