f1dbb7b31d741f636f1e0588c81b4986c8c266a6
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-26 17:14:04 -0300

    [chore] adding restricted api key

---
+++ app/services/constants/api.key.ts
@@ -0,0 +1,1 @@
+export const key = 'AIzaSyA_Wmdq5j_j5PTygb6Xsi4pV0Z27b_VKYY';
--- app/services/http.ts
+++ app/services/http.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { key } from './constants/api.key';
 
 const instance = axios.create({
   baseURL: 'https://www.googleapis.com/books/v1',
   timeout: 1000,
 });
+
+instance.defaults.params = { key };
 
 export default instance;
