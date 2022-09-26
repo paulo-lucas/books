3650a3152f0e7c94fa0309e264413cb9c953ce90
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-26 01:49:25 -0300

    [refactor] optimizing toggle theme button

--- app/components/ToggleTheme/index.tsx
+++ app/components/ToggleTheme/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Icon, useTheme, useThemeMode } from '@rneui/themed';
+import { Icon, useThemeMode } from '@rneui/themed';
 
 export const ToggleTheme = () => {
   const { mode, setMode } = useThemeMode();
-  const {
-    theme: { colors },
-  } = useTheme();
 
   const isDark = () => mode === 'dark';
   const toggle = () => setMode(isDark() ? 'light' : 'dark');
 
   return (
-    <Icon
-      name={isDark() ? 'moon' : 'sun'}
-      type="feather"
-      color={colors.black}
-      onPress={toggle}
-    />
+    <Icon name={isDark() ? 'moon' : 'sun'} type="feather" onPress={toggle} />
   );
 };
