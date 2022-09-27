c0eed150f33e920683d62b47db13c3e438821888
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-27 01:49:43 -0300

    [feat] book info

--- app/components/BookInfo/index.tsx
+++ app/components/BookInfo/index.tsx
@@ -1,43 +1,84 @@
-import React from 'react';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import { Image, Skeleton } from '@rneui/themed';
 import { useBookDetails } from '@app/hooks';
 import { getBestFromImageLinks } from '@app/utils/getBestImageFromImagelinks';
 import BookInfoSkeleton from './BookInfoSkeleton';
+import { VolumeInfo } from '@app/interfaces/volume';
 
 export const BookInfo = () => {
-  const { book, isFavorite, loading, toggleFavorite } = useBookDetails();
+  const { book, loading } = useBookDetails();
+  const [descriptionOpen, setDescriptionoOpen] = useState<boolean>(false);
+
+  const toggleDescription = () => setDescriptionoOpen(!descriptionOpen);
 
   if (loading) {
     return <BookInfoSkeleton />;
   }
 
-  const image = getBestFromImageLinks(book?.volumeInfo.imageLinks);
-  console.log(image);
+  const info = book?.volumeInfo as VolumeInfo;
+  const image = getBestFromImageLinks(info.imageLinks);
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.imageWrapper}>
-        <Image
-          source={{ uri: image }}
-          style={styles.image}
-          placeholderStyle={styles.placeholder}
-          PlaceholderContent={<Skeleton style={styles.placeholder} />}
-        />
-      </View>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollview}>
+        <View style={styles.imageWrapper}>
+          <Image
+            source={{ uri: image }}
+            style={styles.image}
+            placeholderStyle={styles.placeholder}
+            PlaceholderContent={<Skeleton style={styles.placeholder} />}
+          />
+        </View>
 
-      <Text style={styles.title}>{book?.volumeInfo.title}</Text>
-      <Text style={styles.author}>{book?.volumeInfo.authors.join(', ')}</Text>
-      <Text style={styles.description}>{book?.volumeInfo.description}</Text>
-    </ScrollView>
+        <Text style={styles.title}>{info.title}</Text>
+        <Text style={styles.aditionalInfo}>
+          By {info.authors.length ? info.authors.join(', ') : info.publisher}
+        </Text>
+        <Text style={styles.aditionalInfo}>
+          {info.publishedDate?.replaceAll('-', '/')}
+        </Text>
+
+        <Text style={styles.subtitle}>
+          {info.pageCount && `${info.pageCount} pages`}
+        </Text>
+
+        <Text style={styles.subtitle}>Categories</Text>
+        {info.categories?.map((category, i) => (
+          <Text key={i} style={styles.aditionalInfo}>
+            {category}
+          </Text>
+        ))}
+
+        <Text style={styles.subtitle}>Description</Text>
+        <TouchableOpacity onPress={toggleDescription}>
+          <Text
+            numberOfLines={descriptionOpen ? 100 : 5}
+            style={styles.description}>
+            {info.description}
+          </Text>
+          <Text style={styles.toggleDescription}>
+            {descriptionOpen ? 'Read less' : 'Read more'}
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 40,
     paddingVertical: 20,
+  },
+  scrollview: {
+    paddingHorizontal: 20,
   },
   imageWrapper: {
     width: '100%',
@@ -53,19 +94,24 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   title: {
+    fontWeight: '600',
     marginTop: 20,
     fontSize: 24,
-    width: '85%',
   },
-  author: {
-    marginTop: 10,
-    marginBottom: 20,
+  aditionalInfo: {
+    fontSize: 14,
+  },
+  subtitle: {
+    fontWeight: '500',
+    marginTop: 20,
     fontSize: 18,
-    width: '60%',
   },
   description: {
-    marginTop: 5,
-    fontSize: 20,
-    width: '100%',
+    fontSize: 16,
+  },
+  toggleDescription: {
+    fontSize: 16,
+    fontWeight: '500',
+    textDecorationLine: 'underline',
   },
 });
