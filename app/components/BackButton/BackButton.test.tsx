c2e4607d817e1f0287a488e652a17067578b43e1
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-28 22:28:58 -0300

    [fix] removing unused mock

--- app/components/BackButton/BackButton.test.tsx
+++ app/components/BackButton/BackButton.test.tsx
@@ -3,20 +3,6 @@ import React from 'react';
 import { render, screen } from '../../../tests/test-utils';
 import { BackButton } from './index';
 
-const mockGoBack = jest.fn();
-
-jest.mock('@react-navigation/native', () => {
-  const actualRNavigation = jest.requireActual('@react-navigation/native');
-  return {
-    ...actualRNavigation,
-    useNavigation: () => ({
-      navigation: {
-        goBack: mockGoBack,
-      },
-    }),
-  };
-});
-
 describe('back button', () => {
   it('should render correctly', async () => {
     render(<BackButton />);
