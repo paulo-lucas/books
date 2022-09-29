c0f2a0d2194f76d37e6c04be89ec001513213fee
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-29 04:31:01 -0300

    [test] moving app/services mock out of test utils

--- app/contexts/BooksContext/BooksContext.test.tsx
+++ app/contexts/BooksContext/BooksContext.test.tsx
@@ -12,6 +12,44 @@ import { Bookshelf, SearchBar } from '@app/components';
 import Home from '@app/screens/Home';
 jest.useFakeTimers();
 
+jest.mock('@app/services', () => ({
+  searchVolumes: () => ({
+    data: {
+      items: [
+        {
+          id: '2Y3iBgAAQBAJ',
+          volumeInfo: {
+            title: 'Descubra Nárnia',
+            authors: ['Christin Ditchfield'],
+            publisher: 'RBC Publicações',
+            publishedDate: '2010-04-02',
+            description: 'O Leão, a Feiticeira e o Guarda-Roupas',
+
+            pageCount: 256,
+            categories: ['Religion'],
+            imageLinks: {
+              smallThumbnail:
+                'http://books.google.com/books/content?id=2Y3iBgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+              thumbnail:
+                'http://books.google.com/books/content?id=2Y3iBgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+            },
+          },
+        },
+        {
+          id: 'I2QvPwAACAAJ',
+          volumeInfo: {
+            title: 'As crônicas de Nárnia',
+            authors: ['Clive Staples Lewis'],
+            publishedDate: '2009',
+            description:
+              'Este livro apresenta as sete crônicas de Nárnia integralmente, num único volume.',
+          },
+        },
+      ],
+    },
+  }),
+}));
+
 const customRender = (ui: ReactElement, options: RenderOptions = {}) => {
   return render(<BooksProvider>{ui}</BooksProvider>, options);
 };
