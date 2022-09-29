bc726a4a9c2cb25e7de733e562cc723a878e3a57
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2022-09-29 02:39:27 -0300

    [test] moving fake timers to global scope

--- app/App.test.tsx
+++ app/App.test.tsx
@@ -2,10 +2,10 @@ import 'react-native';
 import React from 'react';
 import { render, screen, act } from '@testing-library/react-native';
 import App from './App';
+jest.useFakeTimers();
 
 describe('app', () => {
   it('should renders correctly', async () => {
-    jest.useFakeTimers();
     render(<App />);
     act(() => {
       jest.runAllTimers();
